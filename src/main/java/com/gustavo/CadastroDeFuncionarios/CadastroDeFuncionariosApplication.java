5ce5e69a71fc4cdfccc23c813d0f8adbbaffca11
gserapiao <775guga@gmail.com>
2025-05-17 10:54:40 -0300

    Criado a controller

--- src/main/java/com/gustavo/CadastroDeFuncionarios/CadastroDeFuncionariosApplication.java
+++ src/main/java/com/gustavo/CadastroDeFuncionarios/CadastroDeFuncionariosApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroDeFuncionariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeFuncionariosApplication.class, args);
-	}
-
+	public static void main(String[] args) {SpringApplication.run(CadastroDeFuncionariosApplication.class, args);}
+		//https://www.youtube.com/watch?v=n8_qrrc8WN4&t=1673s  54:00 terminei o video neste momento
 }
---
+++ src/main/java/com/gustavo/CadastroDeFuncionarios/Controller.java
@@ -0,0 +1,13 @@
+package com.gustavo.CadastroDeFuncionarios;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Primeira mensagem nesta rota";
+    }
+
+}
