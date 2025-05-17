be5e89eb9dd5a49fca9af9704803e12d0007cde7
gserapiao <775guga@gmail.com>
2025-05-17 11:26:21 -0300

    Correção de nomeação do FuncionarioController

--- src/main/java/com/gustavo/CadastroDeFuncionarios/FuncionarioController.java
+++ src/main/java/com/gustavo/CadastroDeFuncionarios/FuncionarioController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class FuncionarioController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Primeira mensagem nesta rota";
